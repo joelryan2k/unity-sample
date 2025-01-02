fb5cc7f082dfbfbb362b5aefd0f7763d7f64585c
Joel Ryan <joel@iwcenter.com>
2025-01-01 19:44:46 -0500

    optimize

--- Assets/Scripts/PlayerList.cs
+++ Assets/Scripts/PlayerList.cs
@@ -7,17 +7,17 @@ using UnityEngine;
 public class PlayerList : MonoBehaviour
 {
     private NetworkManager _networkManager;
+    private TextMeshProUGUI _textBox;
 
     void Start()
     {
         _networkManager = FindFirstObjectByType<NetworkManager>();
+        _textBox = FindFirstObjectByType<TextMeshProUGUI>();
     }
 
     void Update()
     {
-        var theStupidPlayerList = FindFirstObjectByType<TextMeshProUGUI>();
-
-        theStupidPlayerList.text = String.Join("\n", _networkManager.ClientManager.Clients
+        _textBox.text = String.Join("\n", _networkManager.ClientManager.Clients
             .Where(c => !c.Value.IsHost)
             .Select(c => c.Value.GetAddress().ToString()));
     }
